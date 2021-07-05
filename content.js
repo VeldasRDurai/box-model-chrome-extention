2782dcca41b6c59981a4a6bc3fcbb6fa60aeb718
Veldas R Durai <veldasrdurai72@gmail.com>
2021-07-05 12:08:20 +0530

    some changes and tested

--- content.js
+++ content.js
@@ -1,5 +1,10 @@
 const boxModeling = ( node ) => {
-    node.style.outline = '1px solid green';
+    node.style.backgroundClip = 'content-box';
+    var bgcolor = Math.floor(Math.random()*16777215).toString(16);
+    var color = (0xFFFFFF ^ parseInt( bgcolor.substring(1),16)).toString(16).slice(-6);
+    node.style.backgroundColor = '#'+ bgcolor;
+    node.style.color = '#' + color;
+    node.style.outline = '2px solid #'+ color;
     if( node.childElementCount > 0 ){
         for( child of node.children ){
             boxModeling( child );
@@ -7,7 +12,10 @@ const boxModeling = ( node ) => {
     }
 }
 const cancelBoxModeling = ( node ) => {
-    node.style.outline = 'initial';
+    node.style.outline = '';
+    node.style.backgroundClip = '';
+    node.style.backgroundColor = '';
+    node.style.color = '';
     if( node.childElementCount > 0 ){
         for( child of node.children ){
             cancelBoxModeling( child );
@@ -15,12 +23,5 @@ const cancelBoxModeling = ( node ) => {
     }
 }
 chrome.runtime.sendMessage({todo: "showPageAction"});
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-    if (request.activate){
-        // console.log(' ACTIVATED ');
-        boxModeling(document.body);
-    } else {
-        // console.log(' DEACTIVATED ');
-        cancelBoxModeling(document.body);
-    }
-});
+chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => 
+    request.activate ? boxModeling(document.body) : cancelBoxModeling(document.body) );
